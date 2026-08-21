87d12fcc8f1e3b3ad99a040e478fe998b5c95b86
XLCYun <XLCYun@foxmail.com>
2026-08-21 13:09:36 +0800

    chore: trim bundled desktop icons

--- tests/unit/tauri-resource-layout.test.ts
+++ tests/unit/tauri-resource-layout.test.ts
@@ -14,8 +14,10 @@ describe('Tauri resource layout', () => {
     })
     expect(config.bundle.icon).toEqual([
       'icons/32x32.png',
+      'icons/64x64.png',
       'icons/128x128.png',
       'icons/128x128@2x.png',
+      'icons/icon.png',
       'icons/icon.icns',
       'icons/icon.ico'
     ])
